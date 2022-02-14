0390344648397981c02e3b9fda7149840b2081a5
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-02-14 18:33:54 +0100

    Added: currentYear & myAge

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 $paragraph = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 $censured_word = $_GET['word'];
 $new_paragraph = str_replace($censured_word, '***', $paragraph);
+$currentYear = date("Y");
+$myYear = '1992';
 
 ?>
 
@@ -36,7 +38,11 @@ $new_paragraph = str_replace($censured_word, '***', $paragraph);
         <?php echo strlen($new_paragraph) ?>.
     </p>
 
+    <h4>L'anno corrente è 
+        <?php echo date("Y"); ?>
+    </h4>
 
+    <h5>La mia età è: <?php echo $currentYear - $myYear ?> </h5>
 
 </body>
 
