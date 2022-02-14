2c59efe07fb9648206370b8f9dde63846eeda552
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-02-14 15:52:25 +0100

    Changed: variable string value

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
 $paragraph = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-$censured_word = $_GET['tempor'];
-$new_paragraph = str_replace('tempor', '***', $paragraph);
+$censured_word = $_GET['word'];
+$new_paragraph = str_replace($censured_word, '***', $paragraph);
 
 ?>
 
@@ -30,7 +30,7 @@ $new_paragraph = str_replace('tempor', '***', $paragraph);
     <p>
         <?php echo $new_paragraph ?>
     </p>
-    
+
     <p>
         La lunghezza del mio paragrafo censurato è di (tot. caratteri):
         <?php echo strlen($new_paragraph) ?>.
@@ -50,3 +50,5 @@ Stampare a schermo il paragrafo e la sua lunghezza.
 Una parola da censurare viene passata dall’utente tramite parametro GET.
 Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
  -->
+
+<!-- url -> http://localhost/php-badwords/?word=paroladacensurareconasterischi -->
